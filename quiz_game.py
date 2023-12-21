f6785b300835a12938ca4dc89b2f71b543328fc3
cdeqnb <brianna.mileto@gmail.com>
2023-12-21 14:24:17 -0600

    added .lower() for all answers to be converted into lowercase

--- quiz_game.py
+++ quiz_game.py
@@ -2,37 +2,37 @@ print("Welcome to my animal quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Okay! Let's play! :)")
 
 answer = input("What animal has the thickest fur of any mammal? ")
-if answer == "sea otter":
+if answer.lower() == "sea otter":
     print("Correct!")
 else:
     print("Sorry, that's not correct.")
 
 answer = input("What is a group of cats called? ")
-if answer == "clowder":
+if answer.lower() == "clowder":
     print("Correct!")
 else:
     print("Sorry, that's not correct.")
 
 answer = input("What body part is used to determine the age of a lion? ")
-if answer == "nose":
+if answer.lower() == "nose":
     print("Correct!")
 else:
     print("Sorry, that's not correct.")
 
 answer = input("What is the only mammal capable of true flight? ")
-if answer == "bat":
+if answer.lower() == "bat":
     print("Correct!")
 else:
     print("Sorry, that's not correct.")
 
 answer = input("What is a baby hedgehog called? ")
-if answer == "hoglet":
+if answer.lower() == "hoglet":
     print("Correct!")
 else:
     print("Sorry, that's not correct.")
