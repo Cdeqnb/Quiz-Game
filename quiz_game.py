0f7fb149762d8101fdc6d395e1ece7f66b52feb7
cdeqnb <brianna.mileto@gmail.com>
2023-12-21 14:19:57 -0600

    added spaces after question marks and tested program

--- quiz_game.py
+++ quiz_game.py
@@ -7,31 +7,31 @@ if playing != "yes":
 
 print("Okay! Let's play! :)")
 
-answer = input("What mammal has the thickest fur of any mammal?")
+answer = input("What animal has the thickest fur of any mammal? ")
 if answer == "sea otter":
     print("Correct!")
 else:
     print("Sorry, that's not correct.")
 
-answer = input("What is a group of cats called?")
+answer = input("What is a group of cats called? ")
 if answer == "clowder":
     print("Correct!")
 else:
     print("Sorry, that's not correct.")
 
-answer = input("What body part is used to determine the age of a lion?")
+answer = input("What body part is used to determine the age of a lion? ")
 if answer == "nose":
     print("Correct!")
 else:
     print("Sorry, that's not correct.")
 
-answer = input("What is the only mammal capable of true flight?")
+answer = input("What is the only mammal capable of true flight? ")
 if answer == "bat":
     print("Correct!")
 else:
     print("Sorry, that's not correct.")
 
-answer = input("What is a baby hedgehog called?")
+answer = input("What is a baby hedgehog called? ")
 if answer == "hoglet":
     print("Correct!")
 else:
