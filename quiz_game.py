7018cbb83f6b08c58ecc44a61af08aef5ad93663
cdeqnb <brianna.mileto@gmail.com>
2023-12-21 14:32:14 -0600

    added score

--- quiz_game.py
+++ quiz_game.py
@@ -6,33 +6,41 @@ if playing.lower() != "yes":
     quit()
 
 print("Okay! Let's play! :)")
+score = 0
 
 answer = input("What animal has the thickest fur of any mammal? ")
 if answer.lower() == "sea otter":
     print("Correct!")
+    score += 1
 else:
     print("Sorry, that's not correct.")
 
 answer = input("What is a group of cats called? ")
 if answer.lower() == "clowder":
     print("Correct!")
+    score += 1
 else:
     print("Sorry, that's not correct.")
 
 answer = input("What body part is used to determine the age of a lion? ")
 if answer.lower() == "nose":
     print("Correct!")
+    score += 1
 else:
     print("Sorry, that's not correct.")
 
 answer = input("What is the only mammal capable of true flight? ")
 if answer.lower() == "bat":
     print("Correct!")
+    score += 1
 else:
     print("Sorry, that's not correct.")
 
 answer = input("What is a baby hedgehog called? ")
 if answer.lower() == "hoglet":
     print("Correct!")
+    score += 1
 else:
     print("Sorry, that's not correct.")
+
+print("Congratulations! You got " + str(score) + " questions correct!")
