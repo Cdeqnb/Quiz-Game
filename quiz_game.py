a960cf596c95058c40bd611906093df6fb2a01b4
tanmay-devv <tanmaysharma3141@gmail.com>
2024-02-23 14:52:54 +0530

    updates

--- quiz_game.py
+++ quiz_game.py
@@ -1,3 +1,11 @@
+class answers:
+    first = "sea otter"
+    second = "clowder"
+    third = "nose"
+    fourth = "bat"
+    fifth = "hoglet"
+obj = answers()
+
 print("Welcome to my animal quiz!")
 
 playing = input("Do you want to play? ")
@@ -7,41 +15,48 @@ if playing.lower() != "yes":
 
 print("Okay! Let's play! :)")
 score = 0
+print("Current Score: ", score)
 
 answer = input("What animal has the thickest fur of any mammal? ")
-if answer.lower() == "sea otter":
+if answer.lower() == answers.first:
     print("Correct!")
     score += 1
+    print("Current Score: ", score)
 else:
-    print("Sorry, that's not correct.")
-
+    print("Sorry, the correct answer is: ", answers.first)
+    print("Current Score: ", score)
 answer = input("What is a group of cats called? ")
-if answer.lower() == "clowder":
+if answer.lower() == answers.second:
     print("Correct!")
     score += 1
+    print("Current Score: ", score)
 else:
-    print("Sorry, that's not correct.")
-
+    print("Sorry, the correct answer is: ", answers.second)
+    print("Current Score: ", score)
 answer = input("What body part is used to determine the age of a lion? ")
-if answer.lower() == "nose":
+if answer.lower() == answers.third:
     print("Correct!")
     score += 1
+    print("Current Score: ", score)
 else:
-    print("Sorry, that's not correct.")
-
+    print("Sorry, the correct answer is: ", answers.third)
+    print("Current Score: ", score)
 answer = input("What is the only mammal capable of true flight? ")
-if answer.lower() == "bat":
+if answer.lower() == answers.fourth:
     print("Correct!")
     score += 1
+    print("Current Score: ", score)
 else:
-    print("Sorry, that's not correct.")
-
+    print("Sorry, the correct answer is: ", answers.fourth )
+    print("Current Score: ", score)
 answer = input("What is a baby hedgehog called? ")
-if answer.lower() == "hoglet":
+if answer.lower() == answers.fifth:
     print("Correct!")
     score += 1
+    print("Final Score: ", score)
 else:
-    print("Sorry, that's not correct.")
-
+    print("Sorry, the correct answer is: ", answers.fifth)
+    print("Final Score: ", score)
 print("Congratulations! You got " + str(score) + " questions correct!")
 print("That's " + str(score/5 * 100) + "%!")
+
